3eb8d078575b7b755468bda50ced92f145aaf8af
Ian <ianmmah@gmail.com>
2020-04-15 01:06:36 -0700

    Fix logic

--- index.js
+++ index.js
@@ -4,22 +4,6 @@ addEventListener('fetch', event => {
   event.respondWith(handleRequest(event))
 })
 
-class ElementHandler {
-  constructor(string) {
-    this.string = string
-  }
-  element(element) {
-    element.setInnerContent(this.string);
-  }
-}
-
-class UrlHandler {
-  element(element) {
-    element.setInnerContent('Check out his website');
-    element.setAttribute('href', 'https://ianmah.com/');
-  }
-}
-
 /**
  * Respond with random variant
  */
@@ -39,22 +23,12 @@ async function handleRequest(event) {
     .then(function (response) {
       const { variants } = response
       variant = cookieValue ? cookieValue : getVariantIndex()
-      const url = variants[variant]
+      const url = variants[variant - 1]
       return fetch(url)
     })
     .then(function (content) {
-      const variation = variant
-        ? new HTMLRewriter()
-          .on('title', new ElementHandler('Please Hire'))
-          .on('h1#title', new ElementHandler('To Whom It May Concern'))
-          .on('p#description', new ElementHandler('Please hire Ian Mah.'))
-          .on('a#url', new UrlHandler())
-        : new HTMLRewriter()
-          .on('title', new ElementHandler('Please Hire'))
-          .on('h1#title', new ElementHandler('Dear Hiring Staff'))
-          .on('p#description', new ElementHandler('Please hire Ian Mah.'))
-          .on('a#url', new UrlHandler())
-      return variation.transform(content).text()
+      const rewriter = variant - 1 ? rewriteVariation1() : rewriteVariation2()
+      return rewriter.transform(content).text()
     })
     .then(function (html) {
       return new Response(html, {
@@ -67,8 +41,45 @@ async function handleRequest(event) {
 
 }
 
+class ElementHandler {
+  constructor(string) {
+    this.string = string
+  }
+  element(element) {
+    element.setInnerContent(this.string);
+  }
+}
+
+function rewriteVariation1() {
+  class UrlHandler {
+    element(element) {
+      element.setInnerContent('Check out his website');
+      element.setAttribute('href', 'https://ianmah.com/');
+    }
+  }
+  return new HTMLRewriter()
+    .on('title', new ElementHandler('Please Hire'))
+    .on('h1#title', new ElementHandler('To Whom It May Concern'))
+    .on('p#description', new ElementHandler('Please hire Ian Mah.'))
+    .on('a#url', new UrlHandler())
+}
+
+function rewriteVariation2() {
+  class UrlHandler {
+    element(element) {
+      element.setInnerContent('Click this CTA');
+      element.setAttribute('href', 'https://www.youtube.com/watch?v=dQw4w9WgXcQ');
+    }
+  }
+  return new HTMLRewriter()
+    .on('title', new ElementHandler('Please Hire'))
+    .on('h1#title', new ElementHandler('Yo yo yo'))
+    .on('p#description', new ElementHandler('Please hire Ian Mah!'))
+    .on('a#url', new UrlHandler())
+}
+
 function getVariantIndex() {
-  return (Math.random() < 0.5) ? 0 : 1
+  return (Math.random() < 0.5) ? 1 : 2
 }
 
 function getCookie(cookieString, name) {
