9e8a1316781ef4977de9fd9c86673ed2b66d440b
Ian <ianmmah@gmail.com>
2020-04-14 23:01:56 -0700

    Use HTMLRewriter to reformat the content

--- index.js
+++ index.js
@@ -3,6 +3,32 @@ const API_URL = 'https://cfw-takehome.developers.workers.dev/api/'
 addEventListener('fetch', event => {
   event.respondWith(handleRequest())
 })
+
+class TitleHandler {
+  element(element) {
+    element.setInnerContent('Please Hire Ian Mah');
+  }
+}
+
+class HeaderHandler {
+  element(element) {
+    element.setInnerContent('To Whom It May Concern');
+  }
+}
+
+class DescriptionHandler {
+  element(element) {
+    element.setInnerContent('Please hire Ian Mah.');
+  }
+}
+
+class UrlHandler {
+  element(element) {
+    element.setInnerContent('Check out his website');
+    element.setAttribute('href', 'https://ianmah.com/');
+  }
+}
+
 /**
  * Respond with random variant
  */
@@ -17,6 +43,14 @@ async function handleRequest() {
       const randomIndex = getRandomInt(2)
       const url = variants[randomIndex]
       return fetch(url)
+    })
+    .then(function (data) {
+      return new HTMLRewriter()
+        .on('title', new TitleHandler())
+        .on('h1#title', new HeaderHandler())
+        .on('p#description', new DescriptionHandler())
+        .on('a#url', new UrlHandler())
+        .transform(data)
     })
 
   function getRandomInt(max) {
